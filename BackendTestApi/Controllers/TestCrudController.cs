867ced0eef355b515a1c37f56256fc7c6c61fa68
karlman91 <camargoochoa@hotmail.es>
2024-09-27 00:23:57 -0500

    Backend arquitecture definition

---
+++ BackendTestApi/Controllers/TestCrudController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendTestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestCrudController : ControllerBase
+    {
+    }
+}
